b3c075ad643e04f0b89ab4020b2416382f9c329a
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2024-04-30 14:20:07 +0600

    some more changes

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,17 @@
+"use strict";
 document.addEventListener('DOMContentLoaded', () => {
-    const display = document.getElementById('display') as HTMLInputElement;
-    const errorMsgDiv = document.getElementById(
-        'error-msg-div'
-    ) as HTMLDivElement;
-    const errorMsgSpan = document.getElementById(
-        'error-msg'
-    ) as HTMLSpanElement;
+    const display = document.getElementById('display');
+    const errorMsgDiv = document.getElementById('error-msg-div');
+    const errorMsgSpan = document.getElementById('error-msg');
     let currentExpression = '';
-
     const MIN_VALUE = -9999.99;
     const MAX_VALUE = 9999.99;
-
     // Function to update the display
-    const updateDisplay = (value: string) => {
+    const updateDisplay = (value) => {
         display.value = value;
     };
-
     // Function to validate operand values
-    const isWithinOperandRange = (operand: string) => {
+    const isWithinOperandRange = (operand) => {
         console.log('inside isWithinOperandRange', operand);
         const num = parseFloat(operand);
         if (!isNaN(num)) {
@@ -25,9 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         return false;
     };
-
     // Function to display an error message
-    const displayError = (errorMsg: string) => {
+    const displayError = (errorMsg) => {
         errorMsgDiv.style.display = 'block';
         errorMsgSpan.textContent = errorMsg;
         setTimeout(() => {
@@ -35,12 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
             errorMsgSpan.textContent = '';
         }, 5000);
     };
-
     // Function to extract operands from expression
-    const extractOperands = (expression: string) => {
+    const extractOperands = (expression) => {
         return expression.split(/[-+*/]/).filter(Boolean);
     };
-
     // Add event listeners to each button
     document.querySelectorAll('.calculator .btn').forEach((button) => {
         const value = button.getAttribute('data-value');
@@ -49,58 +40,48 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Temporary expression to check validity
                 const tempExpression = currentExpression + value;
                 const operands = extractOperands(tempExpression);
-
                 console.log('x:', operands);
-
                 // Check if any operand is out of range
-                let isAnyOperandOutOfRange = operands.some(
-                    (operand) => !isWithinOperandRange(operand)
-                );
-
+                let isAnyOperandOutOfRange = operands.some((operand) => !isWithinOperandRange(operand));
                 if (isAnyOperandOutOfRange) {
                     displayError('Operand out of range');
                     disableEqualsButton(true);
-                } else {
+                }
+                else {
                     disableEqualsButton(false);
                 }
-
                 // continue updating the expression
                 currentExpression += value;
                 updateDisplay(currentExpression);
             });
         }
     });
-
     // Equals button event listener
     const equalsButton = document.getElementById('equals');
     if (equalsButton) {
         equalsButton.addEventListener('click', () => {
             try {
                 const result = eval(currentExpression);
-
                 // if infinity then throw error of divide by zero
                 if (result === Infinity || result === -Infinity) {
                     throw new Error('Error: Divide by zero');
                 }
-
                 if (isNaN(result)) {
                     throw new Error('Invalid expression');
                 }
-
                 // if (result < MIN_VALUE || result > MAX_VALUE) {
                 //     throw new Error('Result out of range');
                 // }
-
                 currentExpression = result.toString();
                 updateDisplay(currentExpression);
-            } catch (error) {
+            }
+            catch (error) {
                 currentExpression = '';
                 // alert(error.message);
                 displayError(error.message);
             }
         });
     }
-
     // Clear button event listener
     const clearButton = document.getElementById('clear');
     if (clearButton) {
@@ -110,12 +91,8 @@ document.addEventListener('DOMContentLoaded', () => {
             disableEqualsButton(false);
         });
     }
-
     const disableEqualsButton = (disabled) => {
-        const equalsButton = document.getElementById(
-            'equals'
-        ) as HTMLButtonElement;
-
+        const equalsButton = document.getElementById('equals');
         if (equalsButton) {
             equalsButton.disabled = disabled;
         }
